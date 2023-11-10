1a60193530e0b346921ebdb18fd35419fe701118
iemon_kun <74810160+iemon-kun@users.noreply.github.com>
2023-11-10 22:07:05 +0900

    Add files via upload

---
+++ pwa-tutorial/service-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+});
+
+// 必須記述
+self.addEventListener('fetch', function(event) {});
